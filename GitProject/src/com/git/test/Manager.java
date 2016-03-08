e7d4a046b17866fa7b2143c518d182467d25f448
IDC-020 <IDC-020@IDC-020-PC>
2016-03-08 12:52:26 +0530

    Ani

--- GitProject/src/com/git/test/Manager.java
+++ GitProject/src/com/git/test/Manager.java
@@ -2,7 +2,7 @@ package com.git.test;
 
 public class Manager {
 	public static void main(String[] args) {
-		System.out.println("Hi dear git resolve the issue ");
+		System.out.println("Hi dear git resolve the issue");
 	}
 
 }
